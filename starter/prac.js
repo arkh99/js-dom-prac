786bf3a00d4edc6d3a7b8b9d1c63ab7a0dca8504
arkh99 <arkh919@gmail.com>
2024-12-16 17:47:13 -0500

    done1

--- starter/prac.js
+++ starter/prac.js
@@ -1,14 +1,18 @@
 let user;
 let pass;
 let button = document.getElementById("button");
-button.addEventListener("click", () => {
+button.addEventListener("click", (event) => {
     event.preventDefault();
     user = document.getElementById("user").value
     pass = document.getElementById("pass").value
+    let form = document.getElementById("myform")
     if (!user || !pass) {
         alert("please fill all the information")
     }
     else {
         console.log(`your username is: ${user} and your password is: ${pass}`);
+        form.reset();
+        const name = user.slice(1, user.length)
+        document.getElementById("h1").textContent = `Welcome ${user[0].toUpperCase() + name}`
     }
 })
