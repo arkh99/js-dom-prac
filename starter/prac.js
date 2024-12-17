53fe71003eaa534fc49f209d5c2f205e0304c8c3
arkh99 <arkh919@gmail.com>
2024-12-16 22:14:10 -0500

    math done

--- starter/prac.js
+++ starter/prac.js
@@ -1,1 +1,6 @@
-console.log(Math.sqrt(36));
+let a;
+const btn = document.getElementById("mybtn")
+btn.onclick = function () {
+    a = Math.trunc(Math.random() * 6) + 1;
+    document.getElementById("mylabel").textContent = `Your number is: ${a}`
+}
